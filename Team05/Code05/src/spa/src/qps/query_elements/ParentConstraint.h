1c3d43bb43b72fbe284cc00c5d5cb78b4ffe33d1
LAPTOP-5UFUBL5J\tohzh <toh.zhan.qing@gmail.com>
2024-02-10 20:15:19 +0800

    add ParentConstraints

---
+++ Team05/Code05/src/spa/src/qps/query_elements/ParentConstraint.h
@@ -0,0 +1,21 @@
+//
+// Created by tohzh on 10/2/2024.
+//
+
+#ifndef SPA_PARENTCONSTRAINT_H
+#define SPA_PARENTCONSTRAINT_H
+
+#include "RelationshipConstraint.h"
+#include "StatementReference.h"
+
+class ParentConstraint : public RelationshipConstraint {
+private:
+    std::vector<ConstraintArgument*> constraintArguments;
+public:
+    ParentConstraint(class StatementReference*, class StatementReference*);
+    std::string getConstraintType() override;
+    std::vector<ConstraintArgument*> getConstraintArguments() override;
+};
+
+
+#endif //SPA_PARENTCONSTRAINT_H
--- Team05/Code05/src/spa/src/qps/query_elements/StatementReference.cpp
+++ Team05/Code05/src/spa/src/qps/query_elements/StatementReference.cpp
@@ -5,7 +5,7 @@
 #include "StatementReference.h"
 
 std::string StatementReference::getReferenceType() {
-    return RETURN_TYPE_STATEMENT;
+    return REFERENCE_TYPE_STATEMENT;
 }
 
 std::string StatementReference::getArgumentValue() {
