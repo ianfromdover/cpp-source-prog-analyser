85511c3fc352f7c3b4ad9a59c84aa5f3e336f5f1
Ian <ianfromdover@gmail.com>
2024-02-11 12:50:51 +0800

    fix(pkb): Table Includes to quotes instead of braces

--- Team05/Code05/src/spa/src/pkb/entityTables/ParentTable.h
+++ Team05/Code05/src/spa/src/pkb/entityTables/ParentTable.h
@@ -1,7 +1,7 @@
 #pragma once
 #define SPA_PARENT_TABLE
 
-#include <utilSpa/spaTypes.h>
+#include "utilSpa/spaTypes.h"
 
 class ParentTable {
     // TODO: Use DoubleMap (in stash)
--- Team05/Code05/src/spa/src/pkb/entityTables/StmtTable.h
+++ Team05/Code05/src/spa/src/pkb/entityTables/StmtTable.h
@@ -1,7 +1,7 @@
 #pragma once
 #define SPA_STMTTABLE_H
 
-#include <utilSpa/spaTypes.h>
+#include "utilSpa/spaTypes.h"
 
 class StmtTable {
     // TODO: Use DoubleMap (in stash)
