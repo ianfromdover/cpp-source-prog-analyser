6783fa29888fca4cbaccd5c3ae3a46a19774c71c
LAPTOP-5UFUBL5J\tohzh <toh.zhan.qing@gmail.com>
2024-02-12 21:45:19 +0800

    demo for pkb

--- Team05/Code05/src/spa/src/pkb/Demo.cpp
+++ Team05/Code05/src/spa/src/pkb/Demo.cpp
@@ -6,6 +6,7 @@
 #include "qps/query_elements/constraint_argument/IntegerArgument.h"
 #include "qps/query_elements/constraint_argument/StatementEntity.h"
 #include "qps/query_elements/constraint/ParentConstraint.h"
+#include "utilSpa/SpaTypes.h"
 
 void Demo::demo() {
     //casting ConstraintArgument into intArgument, statementEntity
@@ -13,8 +14,21 @@ void Demo::demo() {
 
     IntegerArgument intArg(5);
     StatementEntity stmtEnt("s1");
-
-
     ParentConstraint c();
+
+    ConstraintArgument a; // you somehow get a constraint argument
+
+    if (a.getReferenceType() == REFERENCE_TYPE_STATEMENT) {
+        //is a reference statement
+        if (a.getEntityType() == RETURN_TYPE_STATEMENT) {
+            // is a statement
+            StatementEntity newStmt = dynamic_cast<StatementEntity>(a);
+            newStmt.value; // get value
+        } else if (a.getEntityType() == RETURN_TYPE_INTEGER) {
+            // is a integer
+            IntegerArgument newInt = dynamic_cast<IntegerArgument>(a);
+            newInt.value; //get value
+        }
+    }
 
 }
