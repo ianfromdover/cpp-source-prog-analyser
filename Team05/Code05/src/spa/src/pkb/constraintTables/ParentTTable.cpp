c0a00be1b138c452c9b83c4d2c7a0a61836dd918
Yew Meng <yewmengchai@gmail.com>
2024-02-21 17:58:53 +0800

    Create ParentTTable API

---
+++ Team05/Code05/src/spa/src/pkb/constraintTables/ParentTTable.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yewme on 21/2/2024.
+//
+
+#include "ParentTTable.h"
---
+++ Team05/Code05/src/spa/src/pkb/constraintTables/ParentTTable.h
@@ -0,0 +1,8 @@
+//
+// Created by yewme on 21/2/2024.
+//
+
+#ifndef SPA_PARENTTTABLE_H
+#define SPA_PARENTTTABLE_H
+
+#endif //SPA_PARENTTTABLE_H
