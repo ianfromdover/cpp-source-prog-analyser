d575e571076f60c294d8881a6cda40ee7d14f1ed
Yew Meng <yewmengchai@gmail.com>
2024-02-21 17:56:43 +0800

    Create FollowsTTable API

---
+++ Team05/Code05/src/spa/src/pkb/constraintTables/FollowsTTable.h
@@ -0,0 +1,37 @@
+//
+// Created by yewme on 21/2/2024.
+//
+
+#ifndef SPA_FOLLOWSTTABLE_H
+#define SPA_FOLLOWSTTABLE_H
+
+#pragma once
+#define SPA_FOLLOWSTABLE_H
+
+#include "utilSpa/SpaTypes.h"
+#include "utilSpa/TwoSideMapManyMany.hpp"
+
+
+class FollowsTable {
+private:
+    TwoSideMapManyMany<StmtNo, StmtNo> twoSideMap;
+public:
+    FollowsTable();
+    // Adds a follows relationship to the follows table, returns true if the relationship is added
+    bool addFollows(StmtNo before, StmtNo after);
+    // Returns true if the before statement follows the after statement
+    bool isFollows(StmtNo before, StmtNo after);
+    // Returns true if there is a statement that is followed by this statement
+    bool hasStmtBefore(StmtNo after);
+    // Returns true if the statement has a follower
+    bool hasFollower(StmtNo before);
+    // Returns the statement that is followed by this statement
+    StmtNo getStmtBefore(StmtNo after);
+    // Returns the follower of this statement. Returns -1 if there is no follower
+    StmtNo getFollower(StmtNo before);
+    // Returns the number of follows relationships
+    [[nodiscard]] int getSize() const;
+};
+
+
+#endif //SPA_FOLLOWSTTABLE_H
