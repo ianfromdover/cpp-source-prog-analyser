57875f69ca78c3f8a27ccf70e5e4a61ec535bef9
Sim Jun Heng <77256455+simjunheng@users.noreply.github.com>
2024-02-22 21:18:00 +0800

    feat(pkb): Add PKB API base class

---
+++ Team05/Code05/src/spa/src/pkb/PKBApi.h
@@ -0,0 +1,38 @@
+//
+// Created by sjh_9 on 22/2/2024.
+//
+
+#ifndef SPA_PKBAPI_H
+#define SPA_PKBAPI_H
+
+#include "utilSpa/SpaTypes.h"
+
+class PKBApi {
+public:
+    virtual bool addRead(StmtNo sNum, VarName name) = 0;
+    virtual bool addCallStmt(StmtNo sNum, ProcName name) = 0;
+    virtual bool addIf(StmtNo sNum, VarName ctrlVarName) = 0;
+    virtual bool addWhile(StmtNo sNum, VarName ctrlVarName) = 0;
+    virtual bool addAssign(StmtNo sNum, VarName LhsVarName) = 0;
+    virtual bool addPrint(StmtNo s, VarName name) = 0;
+    virtual bool addFinalStatementNo(StmtNo s) = 0;
+    virtual bool addProcedure(Str procedureName) = 0; // future: should this have an associated StmtList?
+    virtual bool addVar(StmtNo sNum, VarName name) = 0;
+    virtual bool addConst(StmtNo sNum, ConstVal c) = 0;
+
+    // -- Add Abstractions --
+    virtual bool addFollows(StmtNo before, StmtNo after) = 0;
+    virtual bool addFollowsT(StmtNo before, StmtNo after) = 0;
+    virtual bool addParent(StmtNo parent, StmtNo child) = 0;
+    virtual bool addParentT(StmtNo ancestor, StmtNo descendant) = 0;
+    virtual bool addUses(StmtNo sNum, VarName name) = 0;
+    virtual bool addUses(ProcName ProcedureName, VarName name) = 0;
+    virtual bool addModifies(StmtNo sNum, VarName name) = 0;
+    virtual bool addModifies(ProcName ProcedureName, VarName name) = 0;
+    virtual bool addPatternAsgn(StmtNo sNum, Str lhs, Str rhsBracketed) = 0;
+
+    virtual ~PKBApi() = default; // Ensure there's a virtual destructor
+};
+
+#endif //SPA_PKBAPI_H
+
--- Team05/Code05/src/spa/src/pkb/PopulatePKB.h
+++ Team05/Code05/src/spa/src/pkb/PopulatePKB.h
@@ -3,11 +3,12 @@
 #include <memory>
 //#include "utilSpa/SpaTypes.h"
 #include "pkb/PKBStorage.h"
+#include "PKBApi.h"
 
 using namespace std;
 
 // The interface between PKB and SP
-class PopulatePKB {
+class PopulatePKB : public PKBApi {
 private:
     shared_ptr<PKBStorage> pkb;
 public:
@@ -17,31 +18,31 @@ public:
     // -- Add Entities --
     // relationships
 
-    bool addRead(StmtNo sNum, VarName name);
-    bool addCallStmt(StmtNo sNum, ProcName name);
+    bool addRead(StmtNo sNum, VarName name) override;
+    bool addCallStmt(StmtNo sNum, ProcName name) override;
     // implement using set
-    bool addIf(StmtNo sNum, VarName ctrlVarName);
+    bool addIf(StmtNo sNum, VarName ctrlVarName) override;
     // implement using set
-    bool addWhile(StmtNo sNum, VarName ctrlVarName);
-    bool addAssign(StmtNo sNum, VarName LhsVarName);
-    bool addPrint(StmtNo s, VarName name);
-    bool addFinalStatementNo(StmtNo s);
-    bool addProcedure(Str procedureName); // future: should this have an associated StmtList?
-    bool addVar(StmtNo sNum, VarName name);
-    bool addConst(StmtNo sNum, ConstVal c);
+    bool addWhile(StmtNo sNum, VarName ctrlVarName) override;
+    bool addAssign(StmtNo sNum, VarName LhsVarName) override;
+    bool addPrint(StmtNo s, VarName name) override;
+    bool addFinalStatementNo(StmtNo s) override;
+    bool addProcedure(Str procedureName) override; // future: should this have an associated StmtList?
+    bool addVar(StmtNo sNum, VarName name) override;
+    bool addConst(StmtNo sNum, ConstVal c) override;
 
     // -- Add Abstractions --
     // Adds a follows relationship to the follows table
-    bool addFollows(StmtNo before, StmtNo after);
-    bool addFollowsT(StmtNo before, StmtNo after);
+    bool addFollows(StmtNo before, StmtNo after) override;
+    bool addFollowsT(StmtNo before, StmtNo after) override;
     // Adds a parent relationship to the parent table
-    bool addParent(StmtNo parent, StmtNo child); // parent is s1, child is s2
-    bool addParentT(StmtNo ancestor, StmtNo descendant);
+    bool addParent(StmtNo parent, StmtNo child) override; // parent is s1, child is s2
+    bool addParentT(StmtNo ancestor, StmtNo descendant) override;
     // Adds a uses relationship to the uses table
-    bool addUses(StmtNo sNum, VarName name);
-    bool addUses(ProcName ProcedureName, VarName name);
+    bool addUses(StmtNo sNum, VarName name) override;
+    bool addUses(ProcName ProcedureName, VarName name) override;
     // Adds a modifies relationship to the modifies table
-    bool addModifies(StmtNo sNum, VarName name);
-    bool addModifies(ProcName ProcedureName, VarName name);
-    bool addPatternAsgn(StmtNo sNum, Str lhs, Str rhsBracketed);
+    bool addModifies(StmtNo sNum, VarName name) override;
+    bool addModifies(ProcName ProcedureName, VarName name) override;
+    bool addPatternAsgn(StmtNo sNum, Str lhs, Str rhsBracketed) override;
 };
