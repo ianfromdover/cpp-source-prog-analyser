dd2dfa3f5cdcc8d45f4236598f1f0d83b4e39892
Sim Jun Heng <77256455+simjunheng@users.noreply.github.com>
2024-02-23 03:03:32 +0800

    test(sp): Add unit test for read extractor

--- Team05/Code05/src/unit_testing/src/unit/sp/helper/PKBStubSP.cpp
+++ Team05/Code05/src/unit_testing/src/unit/sp/helper/PKBStubSP.cpp
@@ -1,126 +1,173 @@
+//Ai Link: https://chat.openai.com/share/7135ca30-fc9b-47af-abee-8e458da2587a
+
 #include <iostream>
 #include <set>
+#include <pkb/BasePKBPopulator.h>
 #include "utilSpa/SpaTypes.h"
 
-class PKBStubSP {
+class PKBStubSP : public BasePKBPopulator {
     std::multiset<std::tuple<std::string, std::string, std::string>> tupleCalls;
     std::multiset<std::pair<std::string, std::string>> pairCalls;
     std::multiset<std::string> singleCalls;
 
 public:
-    bool addRead(StmtNo sNum, VarName name) {
+    bool addRead(StmtNo sNum, VarName name) override {
         std::cout << "addRead called" << std::endl;
         pairCalls.insert({std::to_string(sNum), name});
         return true;
     }
 
-    bool addCallStmt(StmtNo sNum, ProcName name) {
+    bool addCallStmt(StmtNo sNum, ProcName name) override {
         std::cout << "addCallStmt called" << std::endl;
         pairCalls.insert({std::to_string(sNum), name});
         return true;
     }
 
-    bool addIf(StmtNo sNum, VarName ctrlVarName) {
+    bool addIf(StmtNo sNum, VarName ctrlVarName) override {
         std::cout << "addIf called" << std::endl;
         pairCalls.insert({std::to_string(sNum), ctrlVarName});
         return true;
     }
 
-    bool addWhile(StmtNo sNum, VarName ctrlVarName) {
+    bool addWhile(StmtNo sNum, VarName ctrlVarName) override {
         std::cout << "addWhile called" << std::endl;
         pairCalls.insert({std::to_string(sNum), ctrlVarName});
         return true;
     }
 
-    bool addAssign(StmtNo sNum, VarName LhsVarName) {
-        std::cout << "addAssign called" << std::endl;
-        pairCalls.insert({std::to_string(sNum), LhsVarName});
-        return true;
-    }
-
-    bool addPrint(StmtNo sNum, VarName name) {
+    bool addPrint(StmtNo sNum, VarName name) override {
         std::cout << "addPrint called" << std::endl;
         pairCalls.insert({std::to_string(sNum), name});
         return true;
     }
 
-    bool addFinalStatementNo(StmtNo sNum) {
+    bool addFinalStatementNo(StmtNo sNum) override {
         std::cout << "addFinalStatementNo called" << std::endl;
         singleCalls.insert(std::to_string(sNum));
         return true;
     }
 
-    bool addProcedure(Str name) {
+    bool addProcedure(Str name) override {
         std::cout << "addProcedure called" << std::endl;
         singleCalls.insert(name);
         return true;
     }
 
-    bool addVar(StmtNo sNum, VarName name) {
+    bool addVar(StmtNo sNum, VarName name) override {
         std::cout << "addVar called" << std::endl;
         pairCalls.insert({std::to_string(sNum), name});
         return true;
     }
 
-    bool addConst(StmtNo sNum, ConstVal c) {
+    bool addConst(StmtNo sNum, ConstVal c) override {
         std::cout << "addConst called" << std::endl;
         pairCalls.insert({std::to_string(sNum), std::to_string(c)});
         return true;
     }
 
-    bool addFollows(StmtNo before, StmtNo after) {
+    bool addFollows(StmtNo before, StmtNo after) override {
         std::cout << "addFollows called" << std::endl;
         pairCalls.insert({std::to_string(before), std::to_string(after)});
         return true;
     }
 
-    bool addFollowsT(StmtNo before, StmtNo after) {
+    bool addFollowsT(StmtNo before, StmtNo after) override {
         std::cout << "addFollowsT called" << std::endl;
         pairCalls.insert({std::to_string(before), std::to_string(after)});
         return true;
     }
 
-    bool addParent(StmtNo parent, StmtNo child) {
+    bool addParent(StmtNo parent, StmtNo child) override {
         std::cout << "addParent called" << std::endl;
         pairCalls.insert({std::to_string(parent), std::to_string(child)});
         return true;
     }
 
-    bool addParentT(StmtNo ancestor, StmtNo descendant) {
+    bool addParentT(StmtNo ancestor, StmtNo descendant) override {
         std::cout << "addParentT called" << std::endl;
         pairCalls.insert({std::to_string(ancestor), std::to_string(descendant)});
         return true;
     }
 
-    bool addUses(StmtNo sNum, VarName name) {
+    bool addUses(StmtNo sNum, VarName name) override {
         std::cout << "addUses called" << std::endl;
         pairCalls.insert({std::to_string(sNum), name});
         return true;
     }
 
-    bool addUses(ProcName ProcedureName, VarName name) {
+    bool addUses(ProcName ProcedureName, VarName name) override {
         std::cout << "addUses (ProcName) called" << std::endl;
         pairCalls.insert({ProcedureName, name});
         return true;
     }
 
-    bool addModifies(StmtNo sNum, VarName name) {
+    bool addModifies(StmtNo sNum, VarName name) override {
         std::cout << "addModifies called" << std::endl;
         pairCalls.insert({std::to_string(sNum), name});
         return true;
     }
 
-    bool addModifies(ProcName ProcedureName, VarName name) {
+    bool addModifies(ProcName ProcedureName, VarName name) override {
         std::cout << "addModifies (ProcName) called" << std::endl;
         pairCalls.insert({ProcedureName, name});
         return true;
     }
 
-    bool addPatternAsgn(StmtNo sNum, Str lhs, Str rhsBracketed) {
+    bool addPatternAsgn(StmtNo sNum, Str lhs, Str rhsBracketed) override {
         std::cout << "addPatternAsgn called" << std::endl;
         tupleCalls.insert({std::to_string(sNum), lhs, rhsBracketed});
         return true;
     }
 
+    bool checkIfExistPair(const std::multiset<std::pair<std::string, std::string>>& pairVector) {
+        // Iterate through each pair in pairVector
+        for (const auto& pair : pairVector) {
+            // Try to find the pair in pairCalls
+            auto it = pairCalls.find(pair);
+            if (it == pairCalls.end()) {
+                // If the pair is not found, return false
+                std::cout << "ss" << std::endl;
+                return false;
+            } else {
+                // If found, erase one instance of that pair from pairCalls
+                pairCalls.erase(it);
+            }
+        }
+        // If all pairs are found and removed, return true
+        return pairCalls.empty();
+    }
 
+    bool checkIfExistSingle(const std::multiset<std::string>& stringVector) {
+        // Iterate through each string in stringVector
+        for (const auto& str : stringVector) {
+            // Try to find the string in singleCalls
+            auto it = singleCalls.find(str);
+            if (it == singleCalls.end()) {
+                // If the string is not found, return false
+                return false;
+            } else {
+                // If found, erase one instance of that string from singleCalls
+                singleCalls.erase(it);
+            }
+        }
+        // If all strings are found and removed, return true
+        return singleCalls.empty();
+    }
+
+    bool checkIfExistTuple(const std::multiset<std::tuple<std::string, std::string, std::string>>& tupleVector) {
+        // Iterate through each tuple in tupleVector
+        for (const auto& tuple : tupleVector) {
+            // Try to find the tuple in tupleCalls
+            auto it = tupleCalls.find(tuple);
+            if (it == tupleCalls.end()) {
+                // If the tuple is not found, return false
+                return false;
+            } else {
+                // If found, erase one instance of that tuple from tupleCalls
+                tupleCalls.erase(it);
+            }
+        }
+        // If all tuples are found and removed, return true
+        return tupleCalls.empty();
+    }
 };
--- Team05/Code05/src/unit_testing/src/unit/sp/test/ReadExtractorTest.cpp
+++ Team05/Code05/src/unit_testing/src/unit/sp/test/ReadExtractorTest.cpp
@@ -2,88 +2,272 @@
 // Created by sjh_9 on 21/2/2024.
 //
 #include "catch.hpp"
-#include "../helper/AstFactory.cpp"
+#include "../helper/AstProcedures.cpp"
 #include "../helper/PKBStubSP.cpp"
 #include "sp/ReadExtractor.h"
+
 
 //testZeroNestingLevel
 
 TEST_CASE("testAssignCallPrintRead") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"3", "t"}
+    };
+    auto procedure = AstProcedures::createAssignCallPrintRead();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 //testSingleNestingLevel
 
 TEST_CASE("testIfElseWithStmtsBeforeAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"3", "t"},
+            {"8", "t"},
+            {"12", "t"},
+    };
+    auto procedure = AstProcedures::createIfElseWithStmtsBeforeAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 TEST_CASE("testIfElseWithStmtsBeforeAfterAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"3", "t"},
+            {"8", "t"},
+            {"12", "t"},
+            {"16", "t"},
+    };
+    auto procedure = AstProcedures::createIfElseWithStmtsBeforeAfterAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 TEST_CASE("testIfElseWithStmtsAfterAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"4", "t"},
+            {"8", "t"},
+            {"12", "t"},
+    };
+    auto procedure = AstProcedures::createIfElseWithStmtsAfterAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 TEST_CASE("testWhileWithStmtsBeforeAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"3", "t"},
+            {"8", "t"},
+    };
+    auto procedure = AstProcedures::createWhileWithStmtsBeforeAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 TEST_CASE("testWhileWithStmtsBeforeAfterAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"3", "t"},
+            {"8", "t"},
+            {"12", "t"},
+    };
+    auto procedure = AstProcedures::createWhileWithStmtsBeforeAfterAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 TEST_CASE("testWhileWithStmtsAfterAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"4", "t"},
+            {"8", "t"},
+    };
+    auto procedure = AstProcedures::createWhileWithStmtsAfterAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 //testDoubleNestingLevel
 
 TEST_CASE("testIfElseWithNestedIfElsePlusNestedStmtsBeforeAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"4", "t"},
+            {"9", "t"},
+            {"13", "t"},
+            {"17", "t"},
+            {"22", "t"},
+            {"26", "t"},
+    };
+    auto procedure = AstProcedures::createIfElseWithNestedIfElsePlusNestedStmtsBeforeAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 TEST_CASE("testIfElseWithNestedIfElsePlusNestedStmtsBeforeAfterAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"4", "t"},
+            {"9", "t"},
+            {"13", "t"},
+            {"17", "t"},
+            {"21", "t"},
+            {"26", "t"},
+            {"30", "t"},
+            {"34", "t"},
+    };
+    auto procedure = AstProcedures::createIfElseWithNestedIfElsePlusNestedStmtsBeforeAfterAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 TEST_CASE("testIfElseWithNestedIfElsePlusNestedStmtsAfterAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"5", "t"},
+            {"9", "t"},
+            {"13", "t"},
+            {"18", "t"},
+            {"22", "t"},
+            {"26", "t"},
+    };
+    auto procedure = AstProcedures::createIfElseWithNestedIfElsePlusNestedStmtsAfterAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 TEST_CASE("testIfElseWithNestedWhilePlusNestedStmtsBeforeAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"4", "t"},
+            {"9", "t"},
+            {"13", "t"},
+            {"18", "t"},
+    };
+    auto procedure = AstProcedures::createIfElseWithNestedWhilePlusNestedStmtsBeforeAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 TEST_CASE("testIfElseWithNestedWhilePlusNestedStmtsBeforeAfterAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"4", "t"},
+            {"9", "t"},
+            {"13", "t"},
+            {"17", "t"},
+            {"22", "t"},
+            {"26", "t"},
+    };
+    auto procedure = AstProcedures::createIfElseWithNestedWhilePlusNestedStmtsBeforeAfterAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 TEST_CASE("testIfElseWithNestedWhilePlusNestedStmtsAfterAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"5", "t"},
+            {"9", "t"},
+            {"14", "t"},
+            {"18", "t"},
+    };
+    auto procedure = AstProcedures::createIfElseWithNestedWhilePlusNestedStmtsAfterAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 TEST_CASE("testWhileWithNestedIfElsePlusNestedStmtsBeforeAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"4", "t"},
+            {"9", "t"},
+            {"13", "t"},
+    };
+    auto procedure = AstProcedures::createWhileWithNestedIfElsePlusNestedStmtsBeforeAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 TEST_CASE("testWhileWithNestedIfElsePlusNestedStmtsBeforeAfterAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"4", "t"},
+            {"9", "t"},
+            {"13", "t"},
+            {"17", "t"},
+    };
+    auto procedure = AstProcedures::createWhileWithNestedIfElsePlusNestedStmtsBeforeAfterAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 TEST_CASE("testWhileWithNestedIfElsePlusNestedStmtsAfterAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"5", "t"},
+            {"9", "t"},
+            {"13", "t"},
+    };
+    auto procedure = AstProcedures::createWhileWithNestedIfElsePlusNestedStmtsAfterAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 TEST_CASE("testWhileWithNestedWhilePlusNestedStmtsBeforeAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"4", "t"},
+            {"9", "t"},
+    };
+    auto procedure = AstProcedures::createWhileWithNestedWhilePlusNestedStmtsBeforeAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 TEST_CASE("testWhileWithNestedWhilePlusNestedStmtsBeforeAfterAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"4", "t"},
+            {"9", "t"},
+            {"13", "t"},
+    };
+    auto procedure = AstProcedures::createWhileWithNestedWhilePlusNestedStmtsBeforeAfterAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
 
 TEST_CASE("testWhileWithNestedWhilePlusNestedStmtsAfterAndInside") {
-    REQUIRE(1==1);
+    std::multiset<pair<std::string, std::string>> resultsVector = {
+            {"5", "t"},
+            {"9", "t"},
+    };
+    auto procedure = AstProcedures::createWhileWithNestedWhilePlusNestedStmtsAfterAndInside();
+    auto pkb = make_shared<PKBStubSP>();
+    shared_ptr<ProgramVisitor> extractor = std::make_shared<ReadExtractor>(pkb);
+    procedure->accept(*extractor);
+    REQUIRE(pkb->checkIfExistPair(resultsVector));
 }
