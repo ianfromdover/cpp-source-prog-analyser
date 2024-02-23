1e22303ac26d8c9de0ba8c7f0a2918b73748c651
LAPTOP-5UFUBL5J\tohzh <toh.zhan.qing@gmail.com>
2024-02-23 15:06:03 +0800

    c

--- Team05/Code05/src/spa/src/qps/QueryEvaluator/QueryEvaluator.cpp
+++ Team05/Code05/src/spa/src/qps/QueryEvaluator/QueryEvaluator.cpp
@@ -29,6 +29,10 @@ std::shared_ptr<Formattable> QueryEvaluator::evaluate(QueryObject & query) {
     std::vector<shared_ptr<QueryResult>> listOfResults;
     listOfResults.reserve(constraints.size());
 
+//    if (constraints.empty()) {
+//        return pkb.getResult(*r, nullptr);
+//    }
+
     //query pkb and store all results into a listOfResults
     for (std::shared_ptr<Constraint> c : constraints) {
             listOfResults.push_back(pkb.getResult(*r, *c));
