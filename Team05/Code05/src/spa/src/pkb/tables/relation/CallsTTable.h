9153c56f5836e06dc133626b9cb00c49700ddd65
Yew Meng <yewmengchai@gmail.com>
2024-03-05 23:21:20 +0800

    feat(pkb): Add Calls and CallsT Tables and API

---
+++ Team05/Code05/src/spa/src/pkb/tables/relation/CallsTTable.h
@@ -0,0 +1,16 @@
+//
+// Created by yewme on 5/3/2024.
+//
+
+#ifndef SPA_CALLSTTABLE_H
+#define SPA_CALLSTTABLE_H
+
+#include "pkb/tables/base/Table.h"
+
+class CallsTTable : public Table {
+
+public:
+    CallsTTable() = default;
+};
+
+#endif //SPA_CALLSTTABLE_H
