8aecf60d1f54869397d51cbaf05ea42d4c1b0788
Ian <ianfromdover@gmail.com>
2024-03-06 18:23:07 +0800

    chore(pkb): Rename TwoSideMap test file

--- Team05/Code05/src/unit_testing/src/unit/pkb/storage/TwoSideMapTest.cpp
+++ Team05/Code05/src/unit_testing/src/unit/pkb/storage/TwoSideMapTest.cpp
@@ -2,14 +2,14 @@
 #include "catch.hpp"
 using namespace std;
 
-TEST_CASE("[Utils TwoSideMapTwoSet] insert") {
+TEST_CASE("[PKB TwoSideMap] insert") {
     TwoSideMap<int, int> map;
     SECTION("Insert 1, 2, insertion successful") {
         REQUIRE(map.insert(1, 2));
     }
 }
 
-TEST_CASE("[Utils TwoSideMapTwoSet] getKeys") {
+TEST_CASE("[PKB TwoSideMap] getKeys") {
     TwoSideMap<int, int> map;
     map.insert(1, 2);
     SECTION("Insert (1,2), get key 2 gives 1") {
@@ -21,7 +21,7 @@ TEST_CASE("[Utils TwoSideMapTwoSet] getKeys") {
 // ai-gen start (copilot, 1, e)
 // prompt: used copilot
 // generate the getValues test just like the getKey one
-TEST_CASE("[Utils TwoSideMapTwoSet] getValues") {
+TEST_CASE("[PKB TwoSideMap] getValues") {
     TwoSideMap<int, int> map;
     map.insert(1, 2);
     SECTION("Insert (1,2), get values 1 gives 2") {
