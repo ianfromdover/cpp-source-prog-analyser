3f5a706b004fbd50908e0f6b280ec0f733899da0
Yew Meng <yewmengchai@gmail.com>
2024-03-06 19:53:51 +0800

    Remove repeated methods

--- Team05/Code05/src/spa/src/pkb/apis/PopulatePKB.cpp
+++ Team05/Code05/src/spa/src/pkb/apis/PopulatePKB.cpp
@@ -81,11 +81,3 @@ bool PopulatePKB::addCalls(std::string caller, std::string called) {
 bool PopulatePKB::addCallsT(std::string caller, std::string called) {
     return pkb->callsTTable->addRecord(caller, called);
 }
-
-bool PopulatePKB::addCalls(std::string caller, std::string called) {
-    return pkb->callsTable->addRecord(caller, called);
-}
-
-bool PopulatePKB::addCallsT(std::string caller, std::string called) {
-    return pkb->callsTTable->addRecord(caller, called);
-}
