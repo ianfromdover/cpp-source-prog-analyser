4e7bd888fa764045da1cdbdac6adea2b515bc4a1
Yew Meng <yewmengchai@gmail.com>
2024-03-07 16:55:46 +0800

    feat(pkb): Add new methods to QueryPKBVirtual.h, make them virtual and add overrides to the QueryPKB.h methods

--- Team05/Code05/src/spa/src/pkb/apis/QueryPKB.h
+++ Team05/Code05/src/spa/src/pkb/apis/QueryPKB.h
@@ -16,24 +16,24 @@ private:
 public:
     explicit QueryPKB(std::shared_ptr<PKBStorage> p);
 
-//    std::vector<std::vector<std::string>> getRead() override;
-//    std::vector<std::vector<std::string>> getCallStmt() override;
-//    std::vector<std::vector<std::string>> getIf() override;
-//    std::vector<std::vector<std::string>> getWhile() override;
-//    std::vector<std::vector<std::string>> getPrint() override;
-//    std::vector<std::vector<std::string>> getStatement() override;
-//    std::vector<std::vector<std::string>> getProcedure() override;
-//    std::vector<std::vector<std::string>> getVar() override;
-//    std::vector<std::vector<std::string>> getConst() override;
-//    std::vector<std::vector<std::string>> getFollows() override;
-//    std::vector<std::vector<std::string>> getFollowsT() override;
-//    std::vector<std::vector<std::string>> getParent() override;
-//    std::vector<std::vector<std::string>> getParentT() override;
-//    std::vector<std::vector<std::string>> getUses() override;
-//    std::vector<std::vector<std::string>> getModifies() override;
-//    std::vector<std::vector<std::string>> getPatternAsgn() override;
-//    std::vector<std::vector<std::string>> getCalls() override;
-//    std::vector<std::vector<std::string>> getCallsT() override;
+//    std::vector<std::vector<std::string>> getRead() override override;
+//    std::vector<std::vector<std::string>> getCallStmt() override override;
+//    std::vector<std::vector<std::string>> getIf() override override;
+//    std::vector<std::vector<std::string>> getWhile() override override;
+//    std::vector<std::vector<std::string>> getPrint() override override;
+//    std::vector<std::vector<std::string>> getStatement() override override;
+//    std::vector<std::vector<std::string>> getProcedure() override override;
+//    std::vector<std::vector<std::string>> getVar() override override;
+//    std::vector<std::vector<std::string>> getConst() override override;
+//    std::vector<std::vector<std::string>> getFollows() override override;
+//    std::vector<std::vector<std::string>> getFollowsT() override override;
+//    std::vector<std::vector<std::string>> getParent() override override;
+//    std::vector<std::vector<std::string>> getParentT() override override;
+//    std::vector<std::vector<std::string>> getUses() override override;
+//    std::vector<std::vector<std::string>> getModifies() override override;
+//    std::vector<std::vector<std::string>> getPatternAsgn() override override;
+//    std::vector<std::vector<std::string>> getCalls() override override;
+//    std::vector<std::vector<std::string>> getCallsT() override override;
 
     // methods to convert to vec<vec<string>>
     template <typename A>
@@ -41,39 +41,39 @@ public:
     vector<vector<std::string>> toVecVecStr(vector<std::string> vec);
     // TODO: remove the following once confirmed that we don't need it
 //    template <typename A, typename B>
-//    vector<vector<std::string>> toVecVecStr(TwoSideMap<A, B> map);
-//    vector<vector<std::string>> toVecVecStr(string element);
+//    vector<vector<std::string>> toVecVecStr(TwoSideMap<A, B> map) override;
+//    vector<vector<std::string>> toVecVecStr(string element) override;
 
     // refactor get
-    std::vector<std::vector<std::string>> getCallByNum(int StmtNo);
-    std::vector<std::vector<std::string>> getCallByProc(ProcName name);
-    std::vector<std::vector<std::string>> getCallTable();
-    std::vector<std::vector<std::string>> getProcByName(ProcName name);
-    std::vector<std::vector<std::string>> getProcTable();
-    std::vector<std::vector<std::string>> getReadByNum(int StmtNo);
-    std::vector<std::vector<std::string>> getReadByVar(VarName var);
-    std::vector<std::vector<std::string>> getReadTable();
-    std::vector<std::vector<std::string>> getIfByNum(int StmtNo);
-    std::vector<std::vector<std::string>> getIfByVar(VarName var);
-    std::vector<std::vector<std::string>> getIfTable();
-    std::vector<std::vector<std::string>> getWhileByNum(int StmtNo);
-    std::vector<std::vector<std::string>> getWhileByVar(string var);
-    std::vector<std::vector<std::string>> getWhileTable();
-    std::vector<std::vector<std::string>> getPrintByNum(int StmtNo);
-    std::vector<std::vector<std::string>> getPrintByVar(string var);
-    std::vector<std::vector<std::string>> getPrintTable();
-    std::vector<std::vector<std::string>> getStmtByNum(int StmtNo);
-    std::vector<std::vector<std::string>> getStmtTable();
-    std::vector<std::vector<std::string>> getVarByName(string var);
-    std::vector<std::vector<std::string>> getVarTable();
-    std::vector<std::vector<std::string>> getConstByName(string var);
-    std::vector<std::vector<std::string>> getConstTable();
-    std::vector<std::vector<std::string>> getFollowsByBefore(int StmtNo);
-    std::vector<std::vector<std::string>> getFollowsByAfter(int StmtNo);
-    std::vector<std::vector<std::string>> getFollowsTable();
-    std::vector<std::vector<std::string>> getFollowsTByBefore(int StmtNo);
-    std::vector<std::vector<std::string>> getFollowsTByAfter(int StmtNo);
-    std::vector<std::vector<std::string>> getFollowsTTable();
+    std::vector<std::vector<std::string>> getCallByNum(int StmtNo) override;
+    std::vector<std::vector<std::string>> getCallByProc(ProcName name) override;
+    std::vector<std::vector<std::string>> getCallTable() override;
+    std::vector<std::vector<std::string>> getProcByName(ProcName name) override;
+    std::vector<std::vector<std::string>> getProcTable() override;
+    std::vector<std::vector<std::string>> getReadByNum(int StmtNo) override;
+    std::vector<std::vector<std::string>> getReadByVar(VarName var) override;
+    std::vector<std::vector<std::string>> getReadTable() override;
+    std::vector<std::vector<std::string>> getIfByNum(int StmtNo) override;
+    std::vector<std::vector<std::string>> getIfByVar(VarName var) override;
+    std::vector<std::vector<std::string>> getIfTable() override;
+    std::vector<std::vector<std::string>> getWhileByNum(int StmtNo) override;
+    std::vector<std::vector<std::string>> getWhileByVar(string var) override;
+    std::vector<std::vector<std::string>> getWhileTable() override;
+    std::vector<std::vector<std::string>> getPrintByNum(int StmtNo) override;
+    std::vector<std::vector<std::string>> getPrintByVar(string var) override;
+    std::vector<std::vector<std::string>> getPrintTable() override;
+    std::vector<std::vector<std::string>> getStmtByNum(int StmtNo) override;
+    std::vector<std::vector<std::string>> getStmtTable() override;
+    std::vector<std::vector<std::string>> getVarByName(string var) override;
+    std::vector<std::vector<std::string>> getVarTable() override;
+    std::vector<std::vector<std::string>> getConstByName(string var) override;
+    std::vector<std::vector<std::string>> getConstTable() override;
+    std::vector<std::vector<std::string>> getFollowsByBefore(int StmtNo) override;
+    std::vector<std::vector<std::string>> getFollowsByAfter(int StmtNo) override;
+    std::vector<std::vector<std::string>> getFollowsTable() override;
+    std::vector<std::vector<std::string>> getFollowsTByBefore(int StmtNo) override;
+    std::vector<std::vector<std::string>> getFollowsTByAfter(int StmtNo) override;
+    std::vector<std::vector<std::string>> getFollowsTTable() override;
 };
 
 
--- Team05/Code05/src/spa/src/pkb/apis/QueryPKBVirtual.h
+++ Team05/Code05/src/spa/src/pkb/apis/QueryPKBVirtual.h
@@ -14,6 +14,55 @@
 
 class QueryPKBVirtual {
 public:
+//    virtual std::shared_ptr<QueryResult> getResult(Returnable&, std::shared_ptr<Constraint>) = 0 = 0= 0;
+//    virtual std::vector<std::vector<std::string>> getRead() = 0 = 0;
+//    virtual std::vector<std::vector<std::string>> getCallStmt() = 0= 0;
+//    virtual std::vector<std::vector<std::string>> getIf() = 0= 0;
+//    virtual std::vector<std::vector<std::string>> getWhile() = 0= 0;
+//    virtual std::vector<std::vector<std::string>> getPrint() = 0= 0;
+//    virtual std::vector<std::vector<std::string>> getStatement() = 0= 0;
+//    virtual std::vector<std::vector<std::string>> getProcedure() = 0= 0;
+//    virtual std::vector<std::vector<std::string>> getVar() = 0= 0;
+//    virtual std::vector<std::vector<std::string>> getConst() = 0= 0;
+//    virtual std::vector<std::vector<std::string>> getFollows() = 0= 0;
+//    virtual std::vector<std::vector<std::string>> getFollowsT() = 0= 0;
+//    virtual std::vector<std::vector<std::string>> getParent() = 0= 0;
+//    virtual std::vector<std::vector<std::string>> getParentT() = 0= 0;
+//    virtual std::vector<std::vector<std::string>> getUses() = 0= 0;
+//    virtual std::vector<std::vector<std::string>> getModifies() = 0= 0;
+//    virtual std::vector<std::vector<std::string>> getPatternAsgn() = 0 = 0;
+//    virtual std::vector<std::vector<std::string>> getCalls() = 0 = 0;
+//    virtual std::vector<std::vector<std::string>> getCallsT() = 0 = 0;
+
+    virtual std::vector<std::vector<std::string>> getCallByNum(int StmtNo) = 0;
+    virtual std::vector<std::vector<std::string>> getCallByProc(ProcName name) = 0;
+    virtual std::vector<std::vector<std::string>> getCallTable() = 0;
+    virtual std::vector<std::vector<std::string>> getProcByName(ProcName name) = 0;
+    virtual std::vector<std::vector<std::string>> getProcTable() = 0;
+    virtual std::vector<std::vector<std::string>> getReadByNum(int StmtNo) = 0;
+    virtual std::vector<std::vector<std::string>> getReadByVar(VarName var) = 0;
+    virtual std::vector<std::vector<std::string>> getReadTable() = 0;
+    virtual std::vector<std::vector<std::string>> getIfByNum(int StmtNo) = 0;
+    virtual std::vector<std::vector<std::string>> getIfByVar(VarName var) = 0;
+    virtual std::vector<std::vector<std::string>> getIfTable() = 0;
+    virtual std::vector<std::vector<std::string>> getWhileByNum(int StmtNo) = 0;
+    virtual std::vector<std::vector<std::string>> getWhileByVar(string var) = 0;
+    virtual std::vector<std::vector<std::string>> getWhileTable() = 0;
+    virtual std::vector<std::vector<std::string>> getPrintByNum(int StmtNo) = 0;
+    virtual std::vector<std::vector<std::string>> getPrintByVar(string var) = 0;
+    virtual std::vector<std::vector<std::string>> getPrintTable() = 0;
+    virtual std::vector<std::vector<std::string>> getStmtByNum(int StmtNo) = 0;
+    virtual std::vector<std::vector<std::string>> getStmtTable() = 0;
+    virtual std::vector<std::vector<std::string>> getVarByName(string var) = 0;
+    virtual std::vector<std::vector<std::string>> getVarTable() = 0;
+    virtual std::vector<std::vector<std::string>> getConstByName(string var) = 0;
+    virtual std::vector<std::vector<std::string>> getConstTable() = 0;
+    virtual std::vector<std::vector<std::string>> getFollowsByBefore(int StmtNo) = 0;
+    virtual std::vector<std::vector<std::string>> getFollowsByAfter(int StmtNo) = 0;
+    virtual std::vector<std::vector<std::string>> getFollowsTable() = 0;
+    virtual std::vector<std::vector<std::string>> getFollowsTByBefore(int StmtNo) = 0;
+    virtual std::vector<std::vector<std::string>> getFollowsTByAfter(int StmtNo) = 0;
+    virtual std::vector<std::vector<std::string>> getFollowsTTable() = 0;
 //    virtual std::shared_ptr<QueryResult> getResult(Returnable&, std::shared_ptr<Constraint>) = 0= 0;
     virtual std::vector<std::vector<std::string>> getRead() = 0;
     virtual std::vector<std::vector<std::string>> getCallStmt()= 0;
