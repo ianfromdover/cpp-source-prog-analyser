01ae6d12902661d737dae80b3fc1f07c1ad73114
LAPTOP-5UFUBL5J\tohzh <toh.zhan.qing@gmail.com>
2024-03-20 17:30:59 +0800

    integer with

---
+++ Team05/Code05/src/spa/src/qps/query_elements/constraint_argument/with_reference/IntegerWith.h
@@ -0,0 +1,23 @@
+//
+// Created by tohzh on 20/3/2024.
+//
+
+#ifndef SPA_INTEGERWITH_H
+#define SPA_INTEGERWITH_H
+
+#include "WithReference.h"
+
+class IntegerWith : public WithReference {
+private:
+    std::string value;
+public:
+    IntegerWith(std::string);
+    std::string getEntityType() override;
+    std::string toString() override;
+    std::string getArgumentValue() override;
+    std::vector<std::vector<std::string>> getEntityTable(QueryPKBVirtual & pkb) override;
+    std::vector<std::vector<std::string>> getRawTable(QueryPKBVirtual & pkb) override;
+};
+
+
+#endif //SPA_INTEGERWITH_H
