7a613a6d5df783bef51e7702de846db358842b44
Sim Jun Heng <77256455+simjunheng@users.noreply.github.com>
2024-03-21 22:35:28 +0800

    fix(sp): Revert Modifies Extractor visitVariable

--- Team05/Code05/src/spa/src/sp/extractor/relation_extractor/ModifiesExtractor.cpp
+++ Team05/Code05/src/spa/src/sp/extractor/relation_extractor/ModifiesExtractor.cpp
@@ -50,22 +50,12 @@ void ModifiesExtractor::visitBinaryExpr(const Binary& expr, shared_ptr<Accumulat
 
 void ModifiesExtractor::visitVariableExpr(const Variable& expr, shared_ptr<Accumulator>& parentInfo) {
     for (const auto& stmtNo : parentInfo->info) {
-        //std::cout << "pkb.addModifies(" << stmtNo << ", " << expr.getName() << ");" << std::endl;
-        pkb->addModifiesS(stmtNo, expr.getName());
         //std::cout << "pkb.addModifiesS(" << stmtNo << ", " << expr.getName() << ");" << std::endl;
         pkb->addModifiesS(stmtNo, expr.getName());
     }
-}
-
-void ModifiesExtractor::visitLiteralExpr(const Literal& expr, shared_ptr<Accumulator>& parentInfo) {
-    for (const auto& stmtNo : parentInfo->info) {
-        //std::cout << "pkb.addModifies(" << stmtNo << ", " << expr.getName() << ");" << std::endl;
-        pkb->addModifiesS(stmtNo, std::to_string(expr.getValue()));
-    } // missing curly brace
     for (const auto& procName : parentInfo->stringInfo) {
         //std::cout << "pkb.addModifiesP(" << procName << ", " << expr.getName() << ");" << std::endl;
-        // TODO: check with QPS guys about this
-        pkb->addModifiesP(procName, expr.toString());
+        pkb->addModifiesP(procName, expr.getName());
     }
 }
 
--- Team05/Code05/src/unit_testing/src/unit/sp/helper/PkbStubSp.cpp
+++ Team05/Code05/src/unit_testing/src/unit/sp/helper/PkbStubSp.cpp
@@ -91,13 +91,13 @@ public:
     }
 
     bool addModifiesS(StmtNo sNum, VarName name) override {
-        //std::cout << "addModifiesS called" << std::endl;
+        std::cout << "addModifiesS called" << std::endl;
         pairCalls.insert({std::to_string(sNum), name});
         return true;
     }
 
     bool addModifiesP(ProcName procName, VarName name) override {
-        //std::cout << "addModifiesP called" << std::endl;
+        std::cout << "addModifiesP called" << std::endl;
         pairCalls.insert({procName, name});
         return true;
     }
@@ -122,7 +122,7 @@ public:
 
     // TODO: check again, now is just to make it compile
 
-    bool addAffects(int before, int after) override {
+    bool addAffects(int before, int after)  {
         //std::cout << "addAffects called" << std::endl;
         pairCalls.insert({std::to_string(before), std::to_string(after)});
         return true;
@@ -134,7 +134,7 @@ public:
         return true;
     }
 
-    bool addNextT(int before, int after) override {
+    bool addNextT(int before, int after) {
         //std::cout << "addAffects called" << std::endl;
         pairCallsT.insert({std::to_string(before), std::to_string(after)});
         return true;
