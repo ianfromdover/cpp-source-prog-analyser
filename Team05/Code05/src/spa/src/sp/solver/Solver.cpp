28a1787b14fa92508e77da499938c1a23f2de5fc
Chua Bing Quan <chipneverdies@gmail.com>
2024-03-29 06:46:38 +0800

    Move `solve` definition to header file to resolve linker errors

--- Team05/Code05/src/spa/src/sp/solver/Solver.cpp
+++ Team05/Code05/src/spa/src/sp/solver/Solver.cpp
@@ -3,39 +3,3 @@
 //
 
 #include "Solver.h"
-#include <queue>
-
-template <typename T>
-auto Solver<T>::solve(const std::shared_ptr<CFG>& cfg, Meet meet, Transfer transfer, T init) -> std::pair<Facts, Facts> {
-    Facts in;
-    Facts out;
-    std::queue<std::shared_ptr<Block>> worklist;
-
-    in.insert({ cfg->getEntryBlock(), init });
-    for (const auto& block : *cfg->getBlocks()) {
-        out.insert({ block, init });
-        worklist.push(block);
-    }
-
-    while (!worklist.empty()) {
-        const auto& block = worklist.front();
-        worklist.pop();
-
-        auto newIn = init;
-        for (const auto& pred : *block->getPredecessors()) {
-            newIn = meet(out[pred], newIn);
-        }
-        in[block] = newIn;
-
-        const auto newOut = transfer(block, in[block]);
-        // TODO: Ensure that `!=` is correctly overloaded or correctly compares the content between the objects.
-        if (newOut != out[block]) {
-            out[block] = newOut;
-            for (const auto& succ : *block->getSuccessors()) {
-                worklist.push(succ);
-            }
-        }
-    }
-
-    return { std::move(in), std::move(out) };
-}
