ac0a1f40dddba8184c789f6f8d12032c290ff3fb
Chua Bing Quan <chipneverdies@gmail.com>
2024-03-29 18:42:51 +0800

    Add sample test for draft implementation of Affects

--- Team05/Code05/src/unit_testing/src/TestAST.cpp
+++ Team05/Code05/src/unit_testing/src/TestAST.cpp
@@ -9,7 +9,7 @@
 #include "sp/api/formatter/ExprFormatter.h"
 #include "sp/exception/FormatterException.h"
 #include "sp/cfg/CFG.h"
-#include "catch.hpp"
+#include "sp/api/Affects.h"
 
 using namespace std;
 void require(bool b) {
@@ -1910,5 +1910,46 @@ TEST_CASE("test") {
         sp.exec(codeSnippet);
 
         require(true);
+    }
+}
+
+
+TEST_CASE("Test Affects") {
+    const auto source = R"(
+        procedure main {
+            x = 0;
+            y = 1;
+            z = 2;
+
+            if (x == 0) then {
+                x = 3;
+            } else {
+                x = 4;
+            }
+        }
+    )";
+
+    const auto& storage = std::make_shared<PkbStorage>();
+    auto pkb = std::make_shared<PopulatePkb>(storage);
+    auto sp = SourceProcessor(pkb);
+    const auto& tokens = sp.scan(source);
+    const auto& program = sp.parse(tokens);
+    sp.validate(program);
+    const auto& cfgs = CFG::compile(program);
+    auto affects = Affects(cfgs);
+    const auto [in, out] = affects.get(1, 2);
+
+    for (const auto& block : *cfgs->at("main")->getBlocks()) {
+        std::cout << block->toString() << std::endl;
+        std::cout << "In: [" << std::endl;
+        for (const auto& var : in.at(block)) {
+            std::cout << "(" << var.getName() << ", " << std::to_string(var.getStmtNo()) << ")" << std::endl;
+        }
+        std::cout << "]" << std::endl;
+        std::cout << "Out: [" << std::endl;
+        for (const auto& var : out.at(block)) {
+            std::cout << "(" << var.getName() << ", " << std::to_string(var.getStmtNo()) << ")" << std::endl;
+        }
+        std::cout << "]" << std::endl;
     }
 }
