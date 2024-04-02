b533be66f1b6ac3d11f1ab085964f7dbab38441d
Sim Jun Heng <77256455+simjunheng@users.noreply.github.com>
2024-04-02 15:10:59 +0800

    fix(qps): Fix Affects semantic error detection

--- Team05/Code05/src/spa/src/qps/query_validator/CompatibleTypeRule.h
+++ Team05/Code05/src/spa/src/qps/query_validator/CompatibleTypeRule.h
@@ -81,11 +81,8 @@ private:
              {QType::CALL, QType::STMT1, QType::PRINT, QType::READ, QType::WHILE,
               QType::IF, QType::ASSIGN, QType::INTEGER, QType::WILDCARD}}},
           {QType::AFFECTS,
-            {{QType::CALL, QType::STMT1, QType::PRINT, QType::READ, QType::WHILE,
-              QType::IF, QType::ASSIGN, QType::INTEGER, QType::WILDCARD},
-             {QType::CALL, QType::STMT1, QType::PRINT, QType::READ, QType::WHILE,
-              QType::IF, QType::ASSIGN, QType::INTEGER, QType::WILDCARD}}}
-              };
+            {{QType::ASSIGN, QType::INTEGER, QType::WILDCARD},
+             { QType::ASSIGN, QType::INTEGER, QType::WILDCARD}}}};
 
 };
 
