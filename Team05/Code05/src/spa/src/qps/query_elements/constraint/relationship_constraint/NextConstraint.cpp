750c276abc34dde79bb1a9f2f5e3cb74c312f84d
Yew Meng <yewmengchai@gmail.com>
2024-04-07 01:50:14 +0800

    chore(qps): Fix error in NextConstraint.cpp hash function

--- Team05/Code05/src/spa/src/qps/query_elements/constraint/relationship_constraint/NextConstraint.cpp
+++ Team05/Code05/src/spa/src/qps/query_elements/constraint/relationship_constraint/NextConstraint.cpp
@@ -94,7 +94,7 @@ std::size_t NextConstraint::hash() const {
     std::size_t hashValue = 0;
 
     // Combine hash values for both stringVars while maintaining their order
-    hashValue ^= stringHasher(CONSTRAINT_TYPE_FOLLOWS) + HASH_OFFSET + (hashValue << 6) + (hashValue >> 2);
+    hashValue ^= stringHasher(CONSTRAINT_TYPE_NEXT) + HASH_OFFSET + (hashValue << 6) + (hashValue >> 2);
     hashValue ^= stringHasher(s1) + HASH_OFFSET + (hashValue << 6) + (hashValue >> 2);
     hashValue ^= stringHasher(s2) + HASH_OFFSET + (hashValue << 6) + (hashValue >> 2);
 
