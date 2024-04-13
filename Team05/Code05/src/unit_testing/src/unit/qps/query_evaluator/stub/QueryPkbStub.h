8fb9e6327c6e80209a09752e80fb3b9b465e731e
Ian <ianfromdover@gmail.com>
2024-04-13 12:34:38 +0800

    test(pkb): rm commented out chunks

--- Team05/Code05/src/unit_testing/src/unit/qps/query_evaluator/stub/QueryPkbStub.h
+++ Team05/Code05/src/unit_testing/src/unit/qps/query_evaluator/stub/QueryPkbStub.h
@@ -117,13 +117,6 @@ public:
     Table getWhileStmtsByVar(VarName var) override;
 
     // relations --------------------------------------------------------------
-    /*
-    Table getAffectsTable() override; // generated each query
-    Table getAffectsAllAffectors() override;
-    Table getAffectsAllAffected() override;
-    Table getAffectsAffectedByAffector(StmtNo before) override;
-    Table getAffectsAffectorsByAffected(StmtNo after) override;
-     */
 
     Table getCallsTable() override;
     Table getCallsAllCallers() override;
@@ -167,14 +160,6 @@ public:
     Table getNextAllAfter() override;
     Table getNextAfterByBefore(StmtNo before) override;
     Table getNextBeforeByAfter(StmtNo after) override;
-
-    /*
-    Table getNextTTable() override; // generated each query
-    Table getNextTAllBefore() override;
-    Table getNextTAllAfter() override;
-    Table getNextTAfterByBefore(StmtNo before) override;
-    Table getNextTBeforeByAfter(StmtNo after) override;
-     */
 
     Table getParentTable() override;
     Table getParentAllChildren() override;
