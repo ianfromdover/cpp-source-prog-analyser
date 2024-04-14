0ac696df0eb1bb2102e5e6b5e4749c9838f57a5d
Chua Bing Quan <chipneverdies@gmail.com>
2024-04-14 17:41:15 +0800

    Stop extracting uses from print, while, and if statements for Affects

--- Team05/Code05/src/spa/src/sp/api/affects/DefUseExtractor.h
+++ Team05/Code05/src/spa/src/sp/api/affects/DefUseExtractor.h
@@ -23,7 +23,9 @@ private:
     std::shared_ptr<Block> currentBlock;
     std::shared_ptr<QueryPkb> queryPkb;
 private:
-    void extractDefinitionsFromCalls();
+    void resetExtractor();
+    void extractDefsUsesCallsFromCFG(const std::shared_ptr<CFG>& cfg);
+    void extractDefsFromCalls();
     std::pair<Definitions, Uses> generateDefUsePair(const std::shared_ptr<CFG> &cfg);
     void addToBlockVarOccurrencesMap(BlockVarOccurrencesMap& map, const std::string& varName, StmtNo stmtNo);
     void addToDefs(const std::string& varName, StmtNo stmtNo);
@@ -32,14 +34,16 @@ private:
 private:
     void visitProcedure(const Procedure& procedure, std::shared_ptr<Accumulator>& _) override;
     void visitReadStmt(const Read& stmt, std::shared_ptr<Accumulator>& _) override;
-    void visitPrintStmt(const Print& stmt, std::shared_ptr<Accumulator>& _) override;
     void visitCallStmt(const Call& stmt, std::shared_ptr<Accumulator>& _) override;
-    void visitWhileStmt(const While& stmt, std::shared_ptr<Accumulator>& _) override;
-    void visitIfStmt(const If& stmt, std::shared_ptr<Accumulator>& _) override;
     void visitAssignStmt(const Assign& stmt, std::shared_ptr<Accumulator>& _) override;
     void visitBinaryExpr(const Binary& expr, std::shared_ptr<Accumulator>& acc) override;
     void visitVariableExpr(const Variable& expr, std::shared_ptr<Accumulator>& acc) override;
     void visitUnaryExpr(const Unary& expr, std::shared_ptr<Accumulator>& acc) override;
+private:
+    // Uncomment to generalize Affects to support non-assignment statements.
+    // void visitPrintStmt(const Print& stmt, std::shared_ptr<Accumulator>& _) override;
+    // void visitWhileStmt(const While& stmt, std::shared_ptr<Accumulator>& _) override;
+    // void visitIfStmt(const If& stmt, std::shared_ptr<Accumulator>& _) override;
 public:
     explicit DefUseExtractor(const std::shared_ptr<QueryPkb>& queryPkb) : queryPkb(queryPkb) {}
     std::pair<Definitions, Uses> extract(const std::shared_ptr<CFG>& cfg);
